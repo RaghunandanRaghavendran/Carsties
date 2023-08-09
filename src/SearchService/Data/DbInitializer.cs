d1306da104ce868a591e6eecb6890913efca6aee
Raghunandan Raghavendran <raghunandhan.r@outlook.com>
2023-08-09 12:50:12 +0530

    End of section 3

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings.
+            FromConnectionString(app.Configuration
+            .GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+              .Key(x => x.Make, KeyType.Text)
+              .Key(x => x.Model, KeyType.Text)
+              .Key(x => x.Color, KeyType.Text)
+              .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        // removing reading from json and reading it from synchronous calls
+
+        // if (count == 0)
+        // {
+        //     Console.WriteLine("No data - will attempt to seed");
+
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+        //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+        //     await DB.SaveAsync(items);
+        // }
+
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+
+        var item = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine(item.Count + " returned from the auction service");
+
+        if (item.Count > 0) await DB.SaveAsync(item);
+
+    }
+
+}
